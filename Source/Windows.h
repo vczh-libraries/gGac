73962f7a556079bfa682d86802e0f58f424de417
vczh <vczh@outlook.com>
2021-10-19 02:36:39 -0700

    Fix tutorial: Layout/*

---
+++ Source/Windows.h
@@ -0,0 +1,10 @@
+/*
+This file is created for fixing these test apps temporary:
+Layout/Responsive1
+Layout/Responsive2
+Layout/RichTextEmbedding
+*/
+
+#define SW_MAXIMIZE nullptr
+
+inline void ShellExecute(...){}
