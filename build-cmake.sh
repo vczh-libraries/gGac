29252e8c209f683fc6cff699d6cb7507253451d7
roodkcab <chenshuoshi@gmail.com>
2021-10-23 09:33:40 +0800

    resolve #14

--- build-cmake.sh
+++ build-cmake.sh
@@ -6,17 +6,3 @@ rm -rf ./out
 mkdir ./out
 cd ./out
 cmake ..
-
-if [ -d "Tests" ]; then
-    cd Tests
-    GACUI_FOLDERS=(GacUI_HelloWorlds GacUI_Layout GacUI_Xml GacUI_Controls GacUI_ControlTemplate)
-    for i in "${GACUI_FOLDERS[@]}"; do
-        if ! [ -d "$i" ]; then
-            mkdir "$i"
-        fi
-        cd "$i"
-        ln -s "../../../GacUI/Tutorial/$i/UIRes" UIRes
-        cd ..
-    done
-    cd ..
-fi
