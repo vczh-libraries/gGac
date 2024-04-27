bcca51c8a94beb90476b59cbf04a0e2550f8aa2e
roodkcab <chenshuoshi@gmail.com>
2024-04-27 21:42:50 +0800

    remove macro

--- Source/Renderers/GuiGGacElementRenderer.h
+++ Source/Renderers/GuiGGacElementRenderer.h
@@ -17,7 +17,68 @@ namespace vl {
 
 				class GuiGGacElementRenderer : public Object, public IGuiGraphicsRenderer
 				{
-				DEFINE_ELEMENT_RENDERER(GuiGGacElement, GuiGGacElementRenderer, Color)
+				public:
+					class Factory : public Object, public IGuiGraphicsRendererFactory
+					{
+					public:
+						IGuiGraphicsRenderer *Create()
+						{
+							GuiGGacElementRenderer *renderer = new GuiGGacElementRenderer;
+							renderer->factory = this;
+							renderer->element = nullptr;
+							renderer->renderTarget = nullptr;
+							return renderer;
+						}
+					};
+
+				protected:
+					IGuiGraphicsRendererFactory *factory;
+					GuiGGacElement *element;
+					IGGacRenderTarget *renderTarget;
+					Size minSize;
+
+				public:
+					static void Register()
+					{
+						auto manager = GetGuiGraphicsResourceManager();
+						CHECK_ERROR(manager != nullptr, L"SetGuiGraphicsResourceManager must be called before registering element renderers.");
+						manager->RegisterRendererFactory(GuiGGacElement::GetElementType(), Ptr(new typename GuiGGacElementRenderer::Factory));
+					}
+
+					IGuiGraphicsRendererFactory *GetFactory() override
+					{
+						return factory;
+					}
+
+					void Initialize(IGuiGraphicsElement *_element) override
+					{
+						element = dynamic_cast<GuiGGacElement *>(_element);
+						static_cast<GuiGGacElementRenderer *>(this)->InitializeInternal();
+					}
+
+					void Finalize() override
+					{
+						static_cast<GuiGGacElementRenderer *>(this)->FinalizeInternal();
+					}
+
+					void SetRenderTarget(IGuiGraphicsRenderTarget *_renderTarget) override
+					{
+						IGGacRenderTarget *oldRenderTarget = renderTarget;
+						renderTarget = static_cast<IGGacRenderTarget *>(_renderTarget);
+						static_cast<GuiGGacElementRenderer *>(this)->RenderTargetChangedInternal(oldRenderTarget, renderTarget);
+					}
+
+					Size GetMinSize() override
+					{
+						return minSize;
+					}
+
+					GuiGGacElementRenderer();
+					void Render(Rect bounds) override;
+					void OnElementStateChanged() override;
+					void InitializeInternal();
+					void FinalizeInternal();
+					void RenderTargetChangedInternal(IGGacRenderTarget* oldRenderTarget, IGGacRenderTarget* newRenderTarget);
 				};
 
 			}
